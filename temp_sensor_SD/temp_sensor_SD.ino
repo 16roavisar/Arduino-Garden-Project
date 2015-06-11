3ff679b06cd9df3e945c9cd3a7d570975f359666
16roavisar <rotem.avisar@gmail.com>
2015-06-11 06:45:51 +0300

    Updated code

--- temp_sensor_SD/temp_sensor_SD.ino
+++ temp_sensor_SD/temp_sensor_SD.ino
@@ -1,32 +1,25 @@
 //temp sensor code
 //Rotem Avisar
 //2/5/2015
-#include <LiquidCrystal.h>
 #include <SD.h>
 #include <dht.h>
-
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+#include <Wire.h>
+#include <Time.h>
+#include <DS1307RTC.h>
 
 dht DHT;
 #define DHT11_PIN 5
 
-const int temperaturePin = 0;
 const int chipSelect = 8;
 float degreesC;
 float degreesF;
 float humidity;
 int soilMoisture;
 int photoReceptor;
-int chk = DHT.read11(DHT11_PIN);
 
 void setup(){
-  //initializes the port and sets communications speed in 9600 bsp
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  //initializes the port and sets lcd display to 16X2
-  lcd.begin(16, 2);
-  //Prints a message to the LCD
-  lcd.print("Data Collection");
   Serial.print("Initializing SD card...");
   pinMode(10, OUTPUT);
   
@@ -36,55 +29,45 @@ void setup(){
     return;
   }
   Serial.println("card initialized.");
+  delay(3000);
 }
-
-
 void loop(){
-  float voltage;
-  float degreesC;
-  float degreesF;
-
-  voltage = getVoltage(temperaturePin);
+  tmElements_t tm;
+  
   //gets the humidity
   humidity = DHT.humidity, 1;
   
-  // Converts voltage to degrees C
-  degreesC = (voltage - 0.5) * 100.0;
   //converts voltage to degrees C
   degreesC = DHT.temperature, 1;
   
-  // Concerts degrees C to degrees F
   //concerts degrees C to degrees F
   degreesF = degreesC * (9.0/5.0) + 32.0;
   
-  //Uses serial port to print values
-  Serial.print("  C: ");
-  Serial.print(degreesC);
-  Serial.print("  F: ");
-  Serial.println(degreesF);
-  
-  //set the cursor to column 0, line 1
-  lcd.setCursor(0, 1);
-  //prints degrees C
-  lcd.print("C: ");
-  lcd.print(degreesC);
-  //set the cursor to column 0, line 2
-  lcd.setCursor(0, 2);
-  //prints degrees F
-  lcd.print("F: ");
-  lcd.print(degreesF);
-  
-  // Collects data every second
-  delay(1000); 
-}
-
   //gets soil moisture
   soilMoisture = analogRead(0);
   
   //gets soil moisture
   photoReceptor = analogRead(1);
   
+  int chk = DHT.read11(DHT11_PIN);
+  
   String dataString = "";
+  String time = "";
+  
+  if(RTC.read(tm)){
+    time += String(tm.Day);
+    time += String("/");
+    time += String(tm.Month);
+    time += String("/");
+    time += String(tmYearToCalendar(tm.Year));
+    time += String(" ");
+    time += String(tm.Hour);
+    time += String(":");
+    time += String(tm.Minute);
+    time += String(":");
+    time += String(tm.Second);
+    Serial.println(time);
+  }
   
   dataString += String("C: ");
   dataString += String(degreesC);
@@ -97,33 +80,25 @@ void loop(){
   dataString += String(" Photo Receptor: ");
   dataString += String(photoReceptor);
 
-float getVoltage(int pin){
-  return (analogRead(pin) * 0.004882814);
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
   
   if (dataFile) {
+    dataFile.println(time);
     dataFile.println(dataString);
+    dataFile.print("\n");
+    dataFile.println("......................................................................");
+    dataFile.print("\n");
     dataFile.close();
     // print to the serial port too:
+    if(chk == DHTLIB_OK){
     Serial.println(dataString);
+    Serial.print("\n");
+    Serial.println("......................................................................");
+    Serial.print("\n");
+    }
   }
   else {
     Serial.println("error opening datalog.txt");
-  } 
-  if(chk == DHTLIB_OK){
-    //uses serial port to print values
-    Serial.print("  C: ");
-    Serial.print(degreesC);
-    Serial.print("  F: ");
-    Serial.println(degreesF);
-    Serial.print("  Humidity: ");
-    Serial.print(humidity);
-    Serial.print("  Soild Moisture: ");
-    Serial.println(soilMoisture);
-    Serial.print("  Photo Receptor: ");
-    Serial.println(photoReceptor);
   }
   delay(1000);
 }
-
-
